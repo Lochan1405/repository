51f292e3637465abaa1a0b13b7f2c238ed7b1d7f
MihikaUngray <mihika@ungraytech.com>
2025-07-09 17:29:25 +0530

    Save all files

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for
+import requests
+all_posts= requests.get("https://api.npoint.io/733f017ca5882ae2c664").json()
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts = all_posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method=="GET":
+        return render_template("contact.html")
+    elif request.method=="POST":
+        return "Message successfully sent!!!"
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
